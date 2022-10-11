c5661b0af65344fd9754c8e3f29bc93806274498
Shubham <16763337+shubham-v@users.noreply.github.com>
2022-10-12 00:15:30 +0530

    uf

--- src/main/java/Question2.java
+++ src/main/java/Question2.java
@@ -6,7 +6,15 @@ public class Question2 {
 
     // Time: O(), Space: O()
     private class Solution {
-        <T> T solve() {
+
+        Object solve() {
+            return null;
+        }
+    }
+
+    private class Solution2 {
+
+        Object solve() {
             return null;
         }
     }
@@ -15,9 +23,9 @@ public class Question2 {
         final var question = new Question2();
 
         final var solution = question.new Solution();
-//        final var solution = question1.new Solution();
+//        final var solution = question.new Solution2();
 
-        final var result = solution.<Object>solve();
+        final var result = solution.solve();
         System.out.println(
                 result
         );
